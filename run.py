7787639fb68f17058710740b797626a4a1cc763f
unah254 <greisunah@gmail.com>
2018-10-20 13:18:52 +0300

    [chore] fix app error

--- run.py
+++ run.py
@@ -2,8 +2,12 @@ import os
 
 from app import create_app
 
+
+
 #config_name = os.getenv('APP_SETTINGS')
 app = create_app(os.getenv('APP_SETTINGS') or 'development')
+
+
 
 
 if __name__ == '__main__':
