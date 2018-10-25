acc6757bd5df6e5404f3bee4485ed813e5d34002
unah254 <greisunah@gmail.com>
2018-10-25 08:31:00 +0300

    [feature #161471046] Add validators

---
+++ app/api/v2/utils.py
@@ -0,0 +1,24 @@
+import re 
+
+
+class Validators:
+    def valid_product_name(self, name):
+        '''confirming name input has numbers and letters only'''
+        print("validating")
+        regex = "^[a-zA-Z0-9_]+$"
+        return re.match(regex, name)
+
+
+    def valid_password(self, password):
+        """validate for password """
+        # positive look ahead
+        return re.match("^(?=.*[A-Z])(?=.*[a-z])(?=.*[0-9])[a-zA-Z0-9]{8,15}$",
+                        password)
+
+    def valid_email(self, email):
+        """ validate for email """
+        return re.match("^[^@]+@[^@]+\.[^@]+$", email)
+
+    def valid_inputs(self, string_inputs):
+        """ validate for inputs """
+        return re.match("^[a-zA-Z0-9-\._@ `]+$", string_inputs)
