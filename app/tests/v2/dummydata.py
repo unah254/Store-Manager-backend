86d80d9cabad42189364b09813f78c975461121c
unah254 <greisunah@gmail.com>
2018-10-25 06:51:29 +0300

    [chore] Add dictionaries

---
+++ app/tests/v2/dummydata.py
@@ -0,0 +1,35 @@
+
+login_data = {
+    "email": "greisunah@admin.com",
+    "password": "Unah1234"
+}
+login_admin_data = {
+    "email": "admin@gmail.com",
+    "password": "sifuma123"
+}
+
+invalid_product_name = {
+    "name": "***********1",
+    "category": "electronics",
+    "price": 2500
+}
+
+incorects_pass_data = {
+    "email": "unah@gmail.com",
+    "password": "Kimame1235"
+}
+
+invalid_password_data = {
+    "email": "mwanzia@gmail.com",
+    "password": "mandie123"
+}
+
+invalid_email_data = {
+    "email": "me",
+    "password": "Sifuma123",
+    "admin": "True"
+}
+user_doest_not_exist_data = {
+    "email": "user20",
+    "password": "Ramo123"
+}
