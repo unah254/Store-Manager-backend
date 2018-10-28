db004990edf17cefc00adf7b41a587420acd10c5
unah254 <greisunah@gmail.com>
2018-10-28 18:53:37 +0300

    [ feature #161531858] Add sales model

--- app/api/v2/models.py
+++ app/api/v2/models.py
@@ -199,13 +199,13 @@ class ProductItem(StoreDatabase):
         self.save()
         self.close()
 
-    def update(self, product_id,name, price, category):
+    def update(self, id, name, price, category):
         """ update an existing product item """
-
+        
         self.cur.execute(
             """ UPDATE productitems SET name =%s, category =%s, price=%s WHERE id = %s """, (
-                name, category, price, product_id,)
-        )
+                name, category, price, id,)
+                )
         self.save()
         self.close()
 
@@ -218,4 +218,118 @@ class ProductItem(StoreDatabase):
 
         if productitems:
             return [self.map_productitems(productitem) for productitem in productitems]
+        return None
+
+sales=[]
+class SalesRecord(StoreDatabase):
+    
+    def __init__(self, name=None, category=None, price=None, quantitysold=None, amountbrought=None):
+        super().__init__()
+        self.name = name
+        self.category = category
+        self.price = price
+        self.quantitysold = quantitysold
+        self.amountbrought= amountbrought
+        self.date = datetime.now().replace(second=0, microsecond=0)
+
+    def create(self):
+        """ create table salerecords """
+        self.create_table(
+            """
+            CREATE TABLE sales (
+                id serial PRIMARY KEY,
+                name VARCHAR NOT NULL UNIQUE,
+                category TEXT,
+                price INTEGER,
+                quantitysold INTEGER,
+                amountbrought INTEGER,
+                date  TIMESTAMP
+            );
+            """
+        )
+
+    def drop(self):
+        """ drop if table exists """
+        self.drop_table('sales')
+
+    def add(self):
+        """ add salerecord to table"""
+        
+        SQL = "INSERT INTO sales(name, category, price, quantitysold,amountbrought, date) VALUES (%s, %s, %s,%s, %s,%s)"
+        data = (self.name, self.category, self.price, self.quantitysold, self.amountbrought, self.date)
+        self.cur.execute(SQL, data)
+        self.save()
+
+    def map_salesrecord(self, data):
+        """ map salerecord to an object"""
+        salerecord = SalesRecord(
+            name=data[1], category=data[2], price=data[3], quantitysold=data[4], amountbrought=data[5])
+        SalesRecord.id = data[0]
+        SalesRecord.date = data[4]
+        self = salerecord
+
+        return self
+
+    def serialize(self):
+        """ serialize a SalesRecord object to a dictionary"""
+        return dict(
+            id=self.id,
+            name=self.name,
+            category=self.category,
+            price=self.price,
+            quantitysold=self.quantitysold,
+            amountbrought=self.amountbrought,
+            date=str(self.date),
+           
+        )
+
+    def fetch_record_by_id(self, _id):
+        """ fetch record by id """
+        self.cur.execute(
+            "SELECT * FROM sales where id = %s", (_id, ))
+        record = self.cur.fetchone()
+        self.save()
+        self.close()
+
+        if record:
+            return self.map_salesrecord(record)
+        return None
+
+    def fetch_by_name(self, name):
+        """ fetch record by name """
+        self.cur.execute("SELECT * FROM sales where name = %s", (name,))
+        record = self.cur.fetchone()
+
+        if record:
+            return self.map_salesrecord(record)
+        return None
+
+    def delete(self, record_id):
+        """ delete sales record """
+
+        self.cur.execute(
+            "DELETE FROM sales WHERE id = %s", (record_id, )
+        )
+        self.save()
+        self.close()
+
+    # def update(self, id, name, price, category):
+    #     """ update an existing product item """
+        
+    #     self.cur.execute(
+    #         """ UPDATE productitems SET name =%s, category =%s, price=%s WHERE id = %s """, (
+    #             name, category, price, id,)
+    #             )
+    #     self.save()
+    #     self.close()
+
+    def fetch_all_salesrecords(self):
+        """ fetch all  sales record """
+        self.cur.execute("SELECT * FROM sales")
+        salerecord = self.cur.fetchall()
+        self.save()
+        self.close()
+
+        if salerecord:
+            return [self.map_salesrecord(productitem) for productitem in salerecord]
         return None
