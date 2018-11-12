f418539977dd5accb93a89e97731c824df949558
unah254 <greisunah@gmail.com>
2018-11-12 10:33:49 +0300

    [chore] Code refactor

--- app/__init__.py
+++ app/__init__.py
@@ -26,7 +26,7 @@ def create_app(config_name):
     CORS(app)
     # app.config.from_object(app_config[config_name])s
     app.config.from_pyfile('config.py')
-    app.config["JWT_SECRET_KEY"]=os.getenv("JWT_SECRET_KEY")
+    app.config["JWT_SECRET_KEY"]=os.getenv("SECRET_KEY")
     app.config['JWT_BLACKLIST_ENABLED'] = True
     app.config['JWT_BLACKLIST_TOKEN_CHECKS'] = ['access']
     with app.app_context():
